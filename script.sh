97e26400bcdd9a7ed0cba0d52a350203404faeb7
uriapete <uriarteangelo@gmail.com>
2025-11-28 18:22:26 -0800

    add fn

---
+++ script.sh
@@ -0,0 +1,23 @@
+git-brclean() {
+    # fetch and prune all remotes
+    git fetch --all --prune
+
+    # set up string for branch names to skip
+    # skips current branch
+    anti_match="(\\*|^\\+"
+
+    # for each branch name in arguments,
+    # add to skip string
+    for name in "$@"; do
+        echo "keeping branch $name";
+        anti_match+="|$name";
+    done
+
+    # finish skip string
+    anti_match+=")"
+    
+    # get all local merged branch names, pass to next
+    # filter for branch names that are not current or in arguments, pass to next
+    # for each given branch, delete if merged
+    git branch --merged | grep -Ev "$anti_match" | xargs -r git branch -d;
+}
