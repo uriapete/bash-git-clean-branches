47becebc3ba8e42abf6d5018760c343ca86e2cb5
Peter Uriarte <uriarteangelo@gmail.com>
2025-12-20 15:40:04 -0800

    init.sh - add function

--- functions/init.sh
+++ functions/init.sh
@@ -1,1 +1,19 @@
 echo "            loading gitp-init"
+
+gitp-init() {
+    # what name should we use for the directory that will hold gitplus data?
+    DIR_NAME=".gitplus"
+
+    # if our data directory does not exist, then create it
+    if ! [ -d "$DIR_NAME/" ]; then
+        echo "Creating $DIR_NAME in $(pwd)"
+        mkdir "$DIR_NAME/"
+    else
+        echo "$DIR_NAME already exists in $(pwd)"
+    fi
+
+    # check for and create if needed: the list of branches to preserve
+    if ! [ -f "$DIR_NAME/preserve_branches" ]; then
+        touch "$DIR_NAME/preserve_branches";
+    fi
+}
