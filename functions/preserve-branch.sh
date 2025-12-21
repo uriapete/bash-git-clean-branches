453a032ae7e71de66c075a013179d06bde742d76
Peter Uriarte <uriarteangelo@gmail.com>
2025-12-20 17:41:05 -0800

    preserve-branch.sh - soft code the path to .gitplus

--- functions/preserve-branch.sh
+++ functions/preserve-branch.sh
@@ -3,8 +3,10 @@ echo "            loading gitp-presbr"
 # adds the desired branches to .gitplus/preserve_branches
 # to prevent them from being automatically pruned
 gitp-preserve-branch() {
+    DATA_PATH=".gitplus/preserve_branches"
+
     # check if the data file exists. if it does not, prompt and terminate
-    if ! [ -f ".gitplus/preserve_branches" ]; then
+    if ! [ -f "$DATA_PATH" ]; then
         echo "Required GitPlus data not found! Please run gitp-init"
         return
     fi;
