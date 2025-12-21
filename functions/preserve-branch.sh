a4c1fac46f236c48577d07e5e9fe6e29a8f33c33
Peter Uriarte <uriarteangelo@gmail.com>
2025-12-20 16:20:07 -0800

    preserve-branch.sh - begin fn, test first check

--- functions/preserve-branch.sh
+++ functions/preserve-branch.sh
@@ -1,1 +1,13 @@
 echo "            loading gitp-presbr"
+
+# adds the desired branches to .gitplus/preserve_branches
+# to prevent them from being automatically pruned
+gitp-preserve-branch() {
+    # check if the data file exists. if it does not, prompt and terminate
+    if ! [ -f ".gitplus/preserve_branches" ]; then
+        echo "Required GitPlus data not found! Please run gitp-init"
+        return
+    fi;
+
+    echo "pb found"
+}
