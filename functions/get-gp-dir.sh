c2ba00b5a29a0bb78fb790b052edc66a1ff0d5b2
Peter Uriarte <uriarteangelo@gmail.com>
2025-12-20 18:09:17 -0800

    get-gp-dir.sh - echo path of .gitplus or null

--- functions/get-gp-dir.sh
+++ functions/get-gp-dir.sh
@@ -11,8 +11,17 @@ _gitp-getdir() {
 
     # while the current directory is not user home...
     while [ "$DIR" != "$(cd ~ && pwd;)" ]; do
-        # testing if while loop works
-        echo "$DIR"
+        # check if the .gitplus folder is there and return the path if it does
+        if [ -d "$DIR/$GP_PATH" ]; then
+            echo "$DIR/$GP_PATH";
+            return;
+        fi
+
+        # else, go up in the tree
         DIR="$(cd "$DIR/.." && pwd)"
     done
+
+    # if the while loop has closed without a return, we have not found a .gitplus folder
+    # therefore,
+    echo "NULL";
 }
