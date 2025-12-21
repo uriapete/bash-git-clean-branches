b8306a659ff14fb372652e8b886906057c21b83e
Peter Uriarte <uriarteangelo@gmail.com>
2025-12-20 18:42:23 -0800

    preserve-branch.sh - use _gitp-getdir

--- functions/preserve-branch.sh
+++ functions/preserve-branch.sh
@@ -3,16 +3,16 @@ echo "            loading gitp-presbr"
 # adds the desired branches to .gitplus/preserve_branches
 # to prevent them from being automatically pruned
 gitp-preserve-branch() {
-    DATA_PATH=".gitplus/preserve_branches"
+    DATA_PATH="$(_gitp-getdir)"
 
     # check if the data file exists. if it does not, prompt and terminate
-    if ! [ -f "$DATA_PATH" ]; then
+    if [ "$DATA_PATH" = "NULL" ]; then
         echo "Required GitPlus data not found! Please run gitp-init"
         return
     fi;
 
     # for each specified branch name, append to list of branches to preserve
     for BR_NM in "$@"; do
-        echo "$BR_NM" >> $DATA_PATH
+        echo "$BR_NM" >> "$DATA_PATH/preserve_branches"
     done
 }
