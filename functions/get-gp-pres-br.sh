7caed72031a23ed20b90f174226a0e63ba4a9fd3
Peter Uriarte <uriarteangelo@gmail.com>
2025-12-21 18:55:17 -0800

    get-gp-pres-br.sh - implement

--- functions/get-gp-pres-br.sh
+++ functions/get-gp-pres-br.sh
@@ -1,1 +1,20 @@
 echo "            loading _gitp-getpresbr"
+
+_gitp-getpresbr(){
+    # get the path of the gitplus folder
+    DATA_PATH="$(_gitp-getdir)"
+
+    # get the path of the list of branches to preserve
+    BRLI="$DATA_PATH/preserve_branches"
+
+    # if the preserve branches file doesnt exist,
+    # prompt and terminate
+    if ! [ -f "$BRLI" ]; then
+        echo "NULL"
+        return
+    fi
+
+    # if we got here, the file exists.
+    # echo so other functions can capture output.
+    echo "$BRLI"
+}
