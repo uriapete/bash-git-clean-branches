586fb2cb4f23ed84c37c897182a6ee1fae15368e
Peter Uriarte <uriarteangelo@gmail.com>
2025-12-21 19:07:05 -0800

    allow-prune-branch.sh - prepare preserve list file

--- functions/allow-prune-branch.sh
+++ functions/allow-prune-branch.sh
@@ -2,5 +2,13 @@ echo "            loading gitp-allow-prune"
 
 # removes the specified branch from preserve_branches.
 gitp-allow-prune() {
-    command ...
+    # get the path of the list of branches to preserve
+    BRLI="$(_gitp-getpresbr)"
+
+    # check if the data file exists. if it does not, prompt and terminate
+    if [ "$BRLI" = "NULL" ]; then
+        echo "Required GitPlus data not found! Please run gitp-init"
+        return
+    fi;
+    echo "ready to prune"
 }
