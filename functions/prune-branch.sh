21e0d1499b056f5df6d21c53dce373fd7a14bcbd
Peter Uriarte <uriarteangelo@gmail.com>
2025-12-22 00:21:58 -0800

    prune-branch.sh - copy over prune code

--- functions/prune-branch.sh
+++ functions/prune-branch.sh
@@ -1,6 +1,9 @@
 echo "            loading gitp-prunebr"
 
 gitp-prunebr(){
+    # fetch and prune all remotes
+    git fetch --all --prune
+
     # get the path of the list of branches to preserve
     BRLI="$(_gitp-getpresbr)"
 
@@ -10,7 +13,24 @@ gitp-prunebr(){
         return
     fi;
 
+    # set up string for branch names to skip
+    # skips current branch
+    anti_match="(\\*|^\\+"
+
     while read -r ln; do
         echo "Keeping branch $ln";
+        anti_match+="|$ln";
     done < "$BRLI"
+
+    echo "Also keeping current branch";
+
+    # finish skip string
+    anti_match+=")"
+
+    # get all local merged branch names, pass to next
+    # filter for branch names that are not current or in arguments, pass to next
+    # for each given branch, delete if merged
+    git branch --merged | grep -Ev "$anti_match" | xargs -r git branch -d;
+    # btw, this line was taken from StackOverflow:
+    # https://stackoverflow.com/a/6127884
 }
