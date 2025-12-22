d8a91cc37019268c29ca0ce27bfed0a6093cfd33
Peter Uriarte <uriarteangelo@gmail.com>
2025-12-22 00:16:05 -0800

    prune-branch.sh - copy preserve branch getter and read out all preserve branches

--- functions/prune-branch.sh
+++ functions/prune-branch.sh
@@ -1,1 +1,16 @@
 echo "            loading gitp-prunebr"
+
+gitp-prunebr(){
+    # get the path of the list of branches to preserve
+    BRLI="$(_gitp-getpresbr)"
+
+    # check if the data file exists. if it does not, prompt and terminate
+    if [ "$BRLI" = "NULL" ]; then
+        echo "Required GitPlus data not found! Please run gitp-init"
+        return
+    fi;
+
+    while read -r ln; do
+        echo "Keeping branch $ln";
+    done < "$BRLI"
+}
